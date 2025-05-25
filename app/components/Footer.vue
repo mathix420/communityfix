1936eea00758e44129ce8f81a980da251bf694ba
Arnaud Gissinger <agissing@student.42.fr>
2025-05-25 18:00:32 +0200

    feat: update Footer component links and icons, adjust layout for better responsiveness

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,36 +1,37 @@
 <script setup lang="ts">
 const links = [
-  { name: 'Privacy Policy', to: '/privacy-policy' },
-  { name: 'Terms of Service', to: '/terms-of-service' },
-  { name: 'Contact Us', to: '/contact' },
+  { name: 'Privacy', to: '/privacy-policy' },
+  { name: 'Terms', to: '/terms-of-service' },
+  { name: 'Contact', to: '/contact' },
 ]
 
 const socials = [
-  { name: 'Bluesky', icon: 'ri:bluesky-fill', url: 'https://twitter.com/communityfix' },
-  { name: 'GitHub', icon: 'ri:github-fill', url: 'https://github.com/mathix420/communityfix' },
+  { name: 'Bluesky', icon: 'fa6-brands:bluesky', url: 'https://twitter.com/communityfix' },
+  { name: 'GitHub', icon: 'fa6-brands:github', url: 'https://github.com/mathix420/communityfix' },
 ]
 </script>
 
 <template>
-  <div class="flex mt-16 select-none gap-3 w-full items-center">
+  <div class="flex mt-16 select-none w-full items-center">
     <div class="border-t w-full border-gray-200" />
     <p class="font-mono">
       communityfix.org
     </p>
     <div class="border-t w-full border-gray-200" />
   </div>
-  <footer class="flex w-full justify-between items-center p-6 *:w-full">
+  <footer class="flex w-full sm:flex-row flex-col-reverse justify-between gap-3 items-center p-6 *:w-full">
     <UButton
       target="_blank"
       to="https://github.com/mathix420/communityfix"
       variant="link"
       color="neutral"
+      class="sm:justify-start justify-center"
     >
       Published under <span class="font-bold">MIT License</span>
     </UButton>
 
     <!-- links -->
-    <nav class="flex gap-4 justify-center">
+    <nav class="flex gap-3 justify-center">
       <UButton
         v-for="link in links"
         :key="link.name"
@@ -43,7 +44,7 @@ const socials = [
     </nav>
 
     <!-- socials -->
-    <nav class="flex gap-4 justify-end items-center">
+    <nav class="flex gap-4 sm:justify-end justify-center items-center">
       <UButton
         v-for="social in socials"
         :key="social.name"
@@ -51,6 +52,7 @@ const socials = [
         :icon="social.icon"
         target="_blank"
         variant="link"
+        size="xl"
         color="neutral"
       />
     </nav>
