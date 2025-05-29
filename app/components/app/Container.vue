2c074efe06a4240201393d9c74afcee2c85ddfb5
Arnaud Gissinger <agissing@student.42.fr>
2025-05-29 16:46:24 +0200

    fix: update container styles to improve overflow handling

--- app/components/app/Container.vue
+++ app/components/app/Container.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="max-w-3xl mx-auto overflow-hidden w-full mb-auto pt-20 px-4 pb-4">
+  <div class="max-w-3xl mx-auto overflow-x-clip h-fit w-full mb-auto pt-20 px-4 pb-4">
     <slot />
   </div>
 </template>
