5f7b485083d28f9454377d580a8fbe714a163404
Arnaud Gissinger <agissing@student.42.fr>
2025-06-07 18:34:24 +0200

    refactor: update issue card to use NuxtLink for author and solution counts, enhancing navigation

--- app/components/card/Issue.vue
+++ app/components/card/Issue.vue
@@ -69,26 +69,35 @@ function handleShare() {
       </p>
       <div class="flex justify-between flex-wrap gap-2">
         <div class="flex gap-2 my-2 flex-wrap">
-          <div class="flex items-center bg-gray-100 px-2 py-1 flex-wrap gap-2 text-sm rounded-md">
+          <NuxtLink
+            :to="`/user/${issue.author}`"
+            class="flex items-center bg-gray-100 px-2 py-1 flex-wrap gap-2 text-sm rounded-md"
+          >
             <img
               :src="`https://api.dicebear.com/9.x/glass/svg?seed=${issue.author}`"
               alt="Author Avatar"
-              class="w-4 h-4 rounded-full"
+              class="size-4 rounded-full"
             >
             <span class="whitespace-nowrap text-gray-700 font-mono">
               {{ issue.author }}
             </span>
-          </div>
-          <div class="flex items-center flex-wrap gap-1 text-sm">
+          </NuxtLink>
+          <NuxtLink
+            :to="`/issue/${issue.id}/solutions`"
+            class="flex items-center flex-wrap gap-1 text-sm"
+          >
             <span class="px-2 py-1 bg-gray-100 whitespace-nowrap text-gray-700 font-mono rounded-md">
               {{ issue.solutionCount || 0 }} Solutions
             </span>
-          </div>
-          <div class="flex items-center gap-1 text-sm">
+          </NuxtLink>
+          <NuxtLink
+            :to="`/issue/${issue.id}/issues`"
+            class="flex items-center gap-1 text-sm"
+          >
             <span class="px-2 py-1 bg-gray-100 whitespace-nowrap text-gray-700 font-mono rounded-md">
               {{ issue.subIssueCount || 0 }} Sub-issues
             </span>
-          </div>
+          </NuxtLink>
         </div>
         <div class="flex sm:items-center justify-between flex-col sm:flex-row gap-2">
           <div class="flex ml-auto gap-1 sm:justify-baseline w-full sm:w-auto">
