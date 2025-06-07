a935b7f9a4f0bcdee077f55d6c22b8f438982a79
Arnaud Gissinger <agissing@student.42.fr>
2025-06-07 15:14:24 +0200

    refactor: update funding and studies pages with descriptive content and improved layout

--- app/pages/issue/[issueId]/funding.vue
+++ app/pages/issue/[issueId]/funding.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="mt-3 bg-white rounded-lg p-4">
-    funding
+    <p class="text-gray-700">
+      Coming soon...
+    </p>
+
+    <p class="text-gray-500 text-sm">
+      We aim to provide a platform where you can support issues through funding.
+      <br>
+      This will allow you to contribute financially to causes you care about, helping to drive change and support initiatives that matter to you.
+      <br>
+      Non-profit organizations, community projects and for-profit initiatives will be able to receive funding from individuals and organizations based on certain conditions.
+    </p>
   </div>
 </template>
--- app/pages/issue/[issueId]/studies.vue
+++ app/pages/issue/[issueId]/studies.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="mt-3 bg-white rounded-lg p-4">
-    studies
+    <p class="text-gray-700">
+      Coming soon...
+    </p>
+
+    <p class="text-gray-500 text-sm">
+      Here you will be able to view and manage studies related to this issue.
+      <br>
+      Sharing ressources, discussing findings, and collaborating with other researchers will be possible.
+    </p>
   </div>
 </template>
