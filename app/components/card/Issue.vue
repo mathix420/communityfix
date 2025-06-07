ac9b0b61458be84f42158e468ebd655496c1f154
Arnaud Gissinger <agissing@student.42.fr>
2025-06-07 15:05:33 +0200

    refactor: enhance issue card layout with author avatar and improved styling

--- app/components/card/Issue.vue
+++ app/components/card/Issue.vue
@@ -67,39 +67,48 @@ function handleShare() {
       <p class="text-gray-700">
         {{ issue.description }}
       </p>
-      <div class="flex gap-4 my-2">
-        <div class="flex items-center flex-wrap gap-1 text-sm">
-          <span class="px-2 py-1 bg-gray-100 whitespace-nowrap text-gray-700 font-mono rounded-md">
-            {{ issue.solutionCount || 0 }} Solutions
-          </span>
+      <div class="flex justify-between flex-wrap gap-2">
+        <div class="flex gap-2 my-2 flex-wrap">
+          <div class="flex items-center bg-gray-100 px-2 py-1 flex-wrap gap-2 text-sm rounded-md">
+            <img
+              :src="`https://api.dicebear.com/9.x/glass/svg?seed=${issue.author}`"
+              alt="Author Avatar"
+              class="w-4 h-4 rounded-full"
+            >
+            <span class="whitespace-nowrap text-gray-700 font-mono">
+              {{ issue.author }}
+            </span>
+          </div>
+          <div class="flex items-center flex-wrap gap-1 text-sm">
+            <span class="px-2 py-1 bg-gray-100 whitespace-nowrap text-gray-700 font-mono rounded-md">
+              {{ issue.solutionCount || 0 }} Solutions
+            </span>
+          </div>
+          <div class="flex items-center gap-1 text-sm">
+            <span class="px-2 py-1 bg-gray-100 whitespace-nowrap text-gray-700 font-mono rounded-md">
+              {{ issue.subIssueCount || 0 }} Sub-issues
+            </span>
+          </div>
         </div>
-        <div class="flex items-center gap-1 text-sm">
-          <span class="px-2 py-1 bg-gray-100 whitespace-nowrap text-gray-700 font-mono rounded-md">
-            {{ issue.subIssueCount || 0 }} Sub-issues
-          </span>
-        </div>
-      </div>
-
-      <div class="flex sm:items-center justify-between flex-col sm:flex-row gap-2">
-        <span class="text-sm text-gray-500">Posted by {{ issue.author }}</span>
-
-        <div class="flex ml-auto gap-1 sm:justify-baseline w-full sm:w-auto">
-          <UButton
-            leading-icon="lucide:share-2"
-            variant="subtle"
-            class="text-primary-600"
-            @click="handleShare"
-          >
-            Share
-          </UButton>
-          <UButton
-            leading-icon="lucide:plus"
-            variant="subtle"
-            :to="`/issue/${issue.id}/solutions`"
-            class="w-full sm:w-auto text-primary-600"
-          >
-            Propose Solution
-          </UButton>
+        <div class="flex sm:items-center justify-between flex-col sm:flex-row gap-2">
+          <div class="flex ml-auto gap-1 sm:justify-baseline w-full sm:w-auto">
+            <UButton
+              leading-icon="lucide:share-2"
+              variant="subtle"
+              class="text-primary-600"
+              @click="handleShare"
+            >
+              Share
+            </UButton>
+            <UButton
+              leading-icon="lucide:plus"
+              variant="subtle"
+              :to="`/issue/${issue.id}/solutions`"
+              class="w-full sm:w-auto text-primary-600"
+            >
+              Propose Solution
+            </UButton>
+          </div>
         </div>
       </div>
     </div>
